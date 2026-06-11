d0e25c838b788e0d9130d5b9fe9d79cdfde78d67
jespersen <jesper.l.richert@gmail.com>
2026-06-11 02:38:02 +0200

    add test notification.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,63 @@
 use std::time::{Duration, Instant};
+use warptrixy::{Config, CustomAppDto, TextValue, WarptrixyClient};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let host = std::env::var("AWTRIX_WEB_URL").unwrap();
     let user = std::env::var("AWTRIX_USER").unwrap();
     let password = std::env::var("AWTRIX_PASSWORD").unwrap();
     println!("Started Awtrixy for connection {}", host);
 
-    let client = "";
+    let client = warptrixy::WarptrixyClient::new(warptrixy::Config {
+        url: host.to_string(),
+        username: user.to_string(),
+        password: password.to_string(),
+    });
+
+    client
+        .send_notification(warptrixy::CustomAppDto {
+            text: Option::from(TextValue::String("TEST".to_string())),
+            text_case: None,
+            top_text: None,
+            text_offset: None,
+            center: None,
+            color: None,
+            gradient: None,
+            blink_text: None,
+            fade_text: None,
+            background: None,
+            rainbow: None,
+            icon: None,
+            push_icon: None,
+            repeat: None,
+            duration: None,
+            hold: None,
+            sound: None,
+            rtttl: None,
+            loop_sound: None,
+            bar: None,
+            line: None,
+            autoscale: None,
+            bar_bc: None,
+            progress: None,
+            progress_c: None,
+            progress_bc: None,
+            pos: None,
+            draw: None,
+            lifetime: None,
+            lifetime_mode: None,
+            stack: None,
+            wakeup: None,
+            no_scroll: None,
+            clients: None,
+            scroll_speed: None,
+            effect: None,
+            effect_settings: None,
+            save: None,
+            overlay: None,
+        })
+        .await
+        .unwrap();
 
     let now = Instant::now();
     let run_every = Duration::from_secs(10);
