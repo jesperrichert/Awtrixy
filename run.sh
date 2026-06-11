d1bffe0e70247adf909577827119ed57718bffae
jespersen <jesper.l.richert@gmail.com>
2026-06-11 02:21:23 +0200

    default setup for Awtrixy

---
+++ run.sh
@@ -0,0 +1,4 @@
+export AWTRIX_WEB_URL=http://192.168.178.10 && \
+export AWTRIX_USER=jesforge && \
+export AWTRIX_PASSWORD=ZpSIuaCLY3fAE2PF1wMStOb1d && \
+cargo run
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::time::{Duration, Instant};
+
 fn main() {
-    println!("Hello, world!");
+    let host = std::env::var("AWTRIX_WEB_URL").unwrap();
+    let user = std::env::var("AWTRIX_USER").unwrap();
+    let password = std::env::var("AWTRIX_PASSWORD").unwrap();
+    println!("Started Awtrixy for connection {}", host);
+
+    let client = "";
+
+    let now = Instant::now();
+    let run_every = Duration::from_secs(10);
+    let mut next_run = now + run_every;
+    loop {
+        println!("Run Every? {}", (now - next_run).as_secs());
+        if (now - next_run).as_secs() > 1 {
+            next_run = Instant::now() + run_every;
+            println!("Running Scheduler for Display Update")
+        }
+    }
 }
